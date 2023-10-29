fd8030ddef95c0bc9e71b9430fd4dc76123f37b1
Vignesh1234587 <vigneshprabhakaran05@gmail.com>
2023-10-29 14:40:06 -0400

    solutions

--- solutions/src/main/java/frc/robot/Robot.java
+++ solutions/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -19,7 +23,12 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  TalonFX rightBack = new TalonFX(0);
+  TalonFX rightFront = new TalonFX(1);
+  TalonFX leftBack = new TalonFX(2);
+  TalonFX leftFront = new TalonFX(3);
 
+  Joystick joy = new Joystick(0);
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -29,6 +38,10 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+
+    rightBack.follow(rightFront);
+    leftBack.follow(leftFront);
   }
 
   /**
@@ -70,7 +83,9 @@ public class Robot extends TimedRobot {
         // Put default auto code here
         break;
     }
-  }
+    leftFront.set(ControlMode.Position, 10 * 250);
+    rightFront.set(ControlMode.Position, 10 * 250); 
+   }
 
   /** This function is called once when teleop is enabled. */
   @Override
@@ -78,7 +93,12 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+  leftFront.set(ControlMode.PercentOutput, joy.getRawAxis(0));
+  rightFront.set(ControlMode.PercentOutput, joy.getRawAxis(4));
+
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
